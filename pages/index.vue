0c56fc7da088ebd632b249371953125b3a225f58
li.pengjie_202380 <li.pengjie_202380@immomo.com>
2020-12-17 16:34:23 +0800

    init

---
+++ pages/index.vue
@@ -0,0 +1,82 @@
+<!--
+ * @Author: your name
+ * @Date: 2020-12-16 22:41:05
+ * @LastEditTime: 2020-12-16 22:57:44
+ * @LastEditors: your name
+ * @Description: In User Settings Edit
+ * @FilePath: /nuxt-app/pages/index.vue
+-->
+<template>
+  <div class="container">
+    <div>
+      <Logo />
+      <h1 class="title">
+        nuxt-app
+      </h1>
+      <div class="links">
+        <a
+          href="https://nuxtjs.org/"
+          target="_blank"
+          rel="noopener noreferrer"
+          class="button--green"
+        >
+          Documentation
+        </a>
+        <a
+          href="https://github.com/nuxt/nuxt.js"
+          target="_blank"
+          rel="noopener noreferrer"
+          class="button--grey"
+        >
+          GitHub
+        </a>
+        <NuxtLink to="/about">about</NuxtLink>
+      </div>
+    </div>
+  </div>
+</template>
+
+<script>
+export default {}
+</script>
+
+<style>
+.container {
+  margin: 0 auto;
+  min-height: 100vh;
+  display: flex;
+  justify-content: center;
+  align-items: center;
+  text-align: center;
+}
+
+.title {
+  font-family:
+    'Quicksand',
+    'Source Sans Pro',
+    -apple-system,
+    BlinkMacSystemFont,
+    'Segoe UI',
+    Roboto,
+    'Helvetica Neue',
+    Arial,
+    sans-serif;
+  display: block;
+  font-weight: 300;
+  font-size: 100px;
+  color: #35495e;
+  letter-spacing: 1px;
+}
+
+.subtitle {
+  font-weight: 300;
+  font-size: 42px;
+  color: #526488;
+  word-spacing: 5px;
+  padding-bottom: 15px;
+}
+
+.links {
+  padding-top: 15px;
+}
+</style>
